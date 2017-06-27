37b9625b978b24f35483398e9ebd56ea5c071456
zhangjinci <zhangjinci@ppmoney.com>
2017-06-27 19:59:18 +0800

    zjc:修正redisson模板

--- slime-distributed-lock-redisson/src/main/java/org/throwable/redisson/support/RedissonLockAspectRegistrar.java
+++ slime-distributed-lock-redisson/src/main/java/org/throwable/redisson/support/RedissonLockAspectRegistrar.java
@@ -15,14 +15,17 @@ import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
 import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
 import org.throwable.distributed.exception.LockException;
 import org.throwable.distributed.exception.UnMatchedLockKeyException;
 import org.throwable.distributed.support.DistributedLockTargetKeyMatcher;
 import org.throwable.redisson.annotation.RedissonDistributedLock;
 import org.throwable.redisson.annotation.RedissonDistributedLocks;
+import org.throwable.redisson.configuration.RedissonLockProperties;
 import org.throwable.utils.ClazzUtils;
 
 import java.lang.reflect.Method;
@@ -36,198 +39,218 @@ import java.util.concurrent.TimeUnit;
  * @since 2017/6/25 1:47
  */
 @Slf4j
-public class RedissonLockAspectRegistrar implements ImportBeanDefinitionRegistrar {
+public class RedissonLockAspectRegistrar implements ImportBeanDefinitionRegistrar, EnvironmentAware {
 
-	private final DistributedLockTargetKeyMatcher keyMatcher = new DistributedLockTargetKeyMatcher();
+    private String location;
 
-	@Override
-	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
-										BeanDefinitionRegistry registry) {
-		final DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) registry;
-		Advice advice = (MethodInterceptor) invocation -> {
-			Method invocationMethod = invocation.getMethod();
-			ProxyMethodInvocation pmi = (ProxyMethodInvocation) invocation;
-			ProceedingJoinPoint pjp = new MethodInvocationProceedingJoinPoint(pmi);
-			Signature signature = pjp.getSignature();
-			MethodSignature methodSignature = (MethodSignature) signature;
-			return processRedissonLock(invocation, invocationMethod, methodSignature, beanFactory);
-		};
-		registerRedissonLockAspect(advice, beanFactory);
-	}
+    private final DistributedLockTargetKeyMatcher keyMatcher = new DistributedLockTargetKeyMatcher();
 
-	private Object processRedissonLock(MethodInvocation methodInvocation, Method invocationMethod,
-									   MethodSignature methodSignature,
-									   DefaultListableBeanFactory beanFactory) {
-		List<LockInvocation> lockInvocations = buildLockInvocations(invocationMethod, methodSignature, beanFactory, methodInvocation);
-		boolean acquireSuccess = false;
-		try {
-			acquireSuccess = processLockChainAcquire(lockInvocations);
-			if (acquireSuccess) {
-				return methodInvocation.proceed();
-			}
-		} catch (Throwable e) {
-			log.error("RedissonLockAspect processRedissonLock execute failed!!", e);
-			throw new RuntimeException(e);  //unknown exception
-		} finally {
-			if (acquireSuccess) {
-				processLockChainRelease(lockInvocations);
-			}
-		}
-		throw new LockException(String.format("RedissonLockAspect processRedissonLock execute failed for trying to get lock timeout!Target method : %s",
-				invocationMethod.getDeclaringClass().getCanonicalName() + "." + invocationMethod.getName()));
-	}
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.location = environment.getProperty(RedissonLockProperties.PREFIX + ".location");
+    }
 
-	private Set<RedissonDistributedLock> findRedissonLockAnnotations(Method invocationMethod) {
-		Set<RedissonDistributedLock> lockAnnos = new HashSet<>();
-		RedissonDistributedLock anno = AnnotationUtils.findAnnotation(invocationMethod, RedissonDistributedLock.class);
-		if (null != anno) {
-			lockAnnos.add(anno);
-		}
-		RedissonDistributedLocks annos = AnnotationUtils.findAnnotation(invocationMethod, RedissonDistributedLocks.class);
-		if (null != annos) {
-			Collections.addAll(lockAnnos, annos.value());
-		}
-		return lockAnnos;
-	}
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+                                        BeanDefinitionRegistry registry) {
+        final DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) registry;
+        registerRedissonLockFactory(beanFactory);
+        registerRedissonLockTemplate(beanFactory);
+        Advice advice = (MethodInterceptor) invocation -> {
+            Method invocationMethod = invocation.getMethod();
+            ProxyMethodInvocation pmi = (ProxyMethodInvocation) invocation;
+            ProceedingJoinPoint pjp = new MethodInvocationProceedingJoinPoint(pmi);
+            Signature signature = pjp.getSignature();
+            MethodSignature methodSignature = (MethodSignature) signature;
+            return processRedissonLock(invocation, invocationMethod, methodSignature, beanFactory);
+        };
+        registerRedissonLockAspect(advice, beanFactory);
+    }
 
-	private List<LockInvocation> buildLockInvocations(Method invocationMethod, MethodSignature methodSignature,
-													  DefaultListableBeanFactory beanFactory, MethodInvocation methodInvocation) {
-		RedissonLockFactory lockFactory = beanFactory.getBean(RedissonLockFactory.class);
-		List<LockInvocation> lockInvocationsToUse = new LinkedList<>();
-		Set<RedissonDistributedLock> lockAnnosToUse = findRedissonLockAnnotations(invocationMethod);
-		for (RedissonDistributedLock anno : lockAnnosToUse) {
-			lockInvocationsToUse.add(buildLockInvocation(anno, methodSignature, lockFactory, methodInvocation));
-		}
-		lockInvocationsToUse.sort(Comparator.comparingInt(LockInvocation::getOrder)); //sort by order
-		return lockInvocationsToUse;
-	}
+    private void registerRedissonLockFactory(DefaultListableBeanFactory beanFactory) {
+        RedissonLockFactory redissonLockFactory = new RedissonLockFactory(location);
+        beanFactory.registerSingleton("redissonLockFactory", redissonLockFactory);
+    }
 
-	private LockInvocation buildLockInvocation(RedissonDistributedLock anno,
-											   MethodSignature methodSignature,
-											   RedissonLockFactory lockFactory, MethodInvocation methodInvocation) {
-		String lockPathToUse = resolveRealLockPathFromKeyArray(anno, methodSignature, methodInvocation);
-		return new LockInvocation(lockPathToUse, anno.waitTime(), anno.leaseTime(), anno.unit(),
-				anno.isFair(), anno.order(), lockFactory);
-	}
+    private void registerRedissonLockTemplate(DefaultListableBeanFactory beanFactory) {
+        RedissonLockFactory redissonLockFactory = beanFactory.getBean(RedissonLockFactory.class);
+        RedissonLockTemplate redissonLockTemplate = new RedissonLockTemplate(redissonLockFactory);
+        beanFactory.registerSingleton("redissonLockTemplate", redissonLockTemplate);
+    }
 
-	private String resolveRealLockPathFromKeyArray(RedissonDistributedLock anno, MethodSignature methodSignature,
-												   MethodInvocation methodInvocation) {
-		String[] parameterNames = methodSignature.getParameterNames();
-		if (null == parameterNames || parameterNames.length == 0) {
-			throw new UnMatchedLockKeyException("@RedissonDistributedLock must be matched to any parameter key!!!!Method parameters array's length is zero");
-		}
-		String[] keyNameArray = anno.keyNames();
-		if (keyNameArray.length == 0) {
-			throw new UnMatchedLockKeyException("@RedissonDistributedLock must be matched to any parameter key!!!!Annotation keyName array's length is zero");
-		}
-		//key - parameter name, value - parameter value
-		Map<String, Object> args = buildMethodParameterMap(parameterNames, methodInvocation.getArguments());
-		StringBuilder targetLockPath = new StringBuilder(anno.lockPathPrefix());
-		for (String key : keyNameArray) {
-			for (Map.Entry<String, Object> entry : args.entrySet()) {
-				String name = entry.getKey();
-				Object value = entry.getValue();
-				if (name.equals(key)) {
-					if (ClazzUtils.isPrimitive(value.getClass())) {
-						targetLockPath.append(String.valueOf(value)).append(anno.keySeparator());
-					} else {
-						throw new UnMatchedLockKeyException(String.format("Key name <%s> is matched to parameter name,but it is not primitive!", key));
-					}
-				} else {
-					targetLockPath.append(keyMatcher.matchAndReturnLockKeyByTargetObjectAndKeyName(value, key)).append(anno.keySeparator());
-				}
-			}
-		}
-		return targetLockPath.substring(0, targetLockPath.lastIndexOf(anno.keySeparator()));
-	}
+    private Object processRedissonLock(MethodInvocation methodInvocation, Method invocationMethod,
+                                       MethodSignature methodSignature,
+                                       DefaultListableBeanFactory beanFactory) {
+        List<LockInvocation> lockInvocations = buildLockInvocations(invocationMethod, methodSignature, beanFactory, methodInvocation);
+        boolean acquireSuccess = false;
+        try {
+            acquireSuccess = processLockChainAcquire(lockInvocations);
+            if (acquireSuccess) {
+                return methodInvocation.proceed();
+            }
+        } catch (Throwable e) {
+            log.error("RedissonLockAspect processRedissonLock execute failed!!", e);
+            throw new RuntimeException(e);  //unknown exception
+        } finally {
+            if (acquireSuccess) {
+                processLockChainRelease(lockInvocations);
+            }
+        }
+        throw new LockException(String.format("RedissonLockAspect processRedissonLock execute failed for trying to get lock timeout!Target method : %s",
+                invocationMethod.getDeclaringClass().getCanonicalName() + "." + invocationMethod.getName()));
+    }
 
-	private Map<String, Object> buildMethodParameterMap(String[] parameterNames, Object[] args) {
-		Map<String, Object> result = new HashMap<>(parameterNames.length);
-		int index = 0;
-		for (String parameterName : parameterNames) {
-			result.put(parameterName, args[index]);
-			index++;
-		}
-		return result;
-	}
+    private Set<RedissonDistributedLock> findRedissonLockAnnotations(Method invocationMethod) {
+        Set<RedissonDistributedLock> lockAnnos = new HashSet<>();
+        RedissonDistributedLock anno = AnnotationUtils.findAnnotation(invocationMethod, RedissonDistributedLock.class);
+        if (null != anno) {
+            lockAnnos.add(anno);
+        }
+        RedissonDistributedLocks annos = AnnotationUtils.findAnnotation(invocationMethod, RedissonDistributedLocks.class);
+        if (null != annos) {
+            Collections.addAll(lockAnnos, annos.value());
+        }
+        return lockAnnos;
+    }
 
-	private boolean processLockChainAcquire(List<LockInvocation> lockInvocations) {
-		return lockInvocations.stream().allMatch(distributedLockInvocation ->
-				!distributedLockInvocation.isHeldByCurrentThread() && distributedLockInvocation.tryLock());
-	}
+    private List<LockInvocation> buildLockInvocations(Method invocationMethod, MethodSignature methodSignature,
+                                                      DefaultListableBeanFactory beanFactory, MethodInvocation methodInvocation) {
+        RedissonLockFactory lockFactory = beanFactory.getBean(RedissonLockFactory.class);
+        List<LockInvocation> lockInvocationsToUse = new LinkedList<>();
+        Set<RedissonDistributedLock> lockAnnosToUse = findRedissonLockAnnotations(invocationMethod);
+        for (RedissonDistributedLock anno : lockAnnosToUse) {
+            lockInvocationsToUse.add(buildLockInvocation(anno, methodSignature, lockFactory, methodInvocation));
+        }
+        lockInvocationsToUse.sort(Comparator.comparingInt(LockInvocation::getOrder)); //sort by order
+        return lockInvocationsToUse;
+    }
 
-	private void processLockChainRelease(List<LockInvocation> lockInvocations) {
-		Collections.reverse(lockInvocations);
-		lockInvocations.forEach(LockInvocation::release);
-	}
+    private LockInvocation buildLockInvocation(RedissonDistributedLock anno,
+                                               MethodSignature methodSignature,
+                                               RedissonLockFactory lockFactory, MethodInvocation methodInvocation) {
+        String lockPathToUse = resolveRealLockPathFromKeyArray(anno, methodSignature, methodInvocation);
+        return new LockInvocation(lockPathToUse, anno.waitTime(), anno.leaseTime(), anno.unit(),
+                anno.isFair(), anno.order(), lockFactory);
+    }
 
-	private void registerRedissonLockAspect(Advice advice, DefaultListableBeanFactory beanFactory) {
-		AspectJExpressionPointcutAdvisor redissonLockAspect = new AspectJExpressionPointcutAdvisor();
-		redissonLockAspect.setLocation("$$redissonLockAspect##");
-		redissonLockAspect.setExpression("@annotation(org.throwable.redisson.annotation.RedissonDistributedLock) || @annotation(org.throwable.redisson.annotation.RedissonDistributedLocks)");
-		redissonLockAspect.setAdvice(advice);
-		redissonLockAspect.setBeanFactory(beanFactory);
-		beanFactory.registerSingleton("redissonLockAspect", redissonLockAspect);
-	}
+    private String resolveRealLockPathFromKeyArray(RedissonDistributedLock anno, MethodSignature methodSignature,
+                                                   MethodInvocation methodInvocation) {
+        String[] parameterNames = methodSignature.getParameterNames();
+        if (null == parameterNames || parameterNames.length == 0) {
+            throw new UnMatchedLockKeyException("@RedissonDistributedLock must be matched to any parameter key!!!!Method parameters array's length is zero");
+        }
+        String[] keyNameArray = anno.keyNames();
+        if (keyNameArray.length == 0) {
+            throw new UnMatchedLockKeyException("@RedissonDistributedLock must be matched to any parameter key!!!!Annotation keyName array's length is zero");
+        }
+        //key - parameter name, value - parameter value
+        Map<String, Object> args = buildMethodParameterMap(parameterNames, methodInvocation.getArguments());
+        StringBuilder targetLockPath = new StringBuilder(anno.lockPathPrefix());
+        for (String key : keyNameArray) {
+            for (Map.Entry<String, Object> entry : args.entrySet()) {
+                String name = entry.getKey();
+                Object value = entry.getValue();
+                if (name.equals(key)) {
+                    if (ClazzUtils.isPrimitive(value.getClass())) {
+                        targetLockPath.append(String.valueOf(value)).append(anno.keySeparator());
+                    } else {
+                        throw new UnMatchedLockKeyException(String.format("Key name <%s> is matched to parameter name,but it is not primitive!", key));
+                    }
+                } else {
+                    targetLockPath.append(keyMatcher.matchAndReturnLockKeyByTargetObjectAndKeyName(value, key)).append(anno.keySeparator());
+                }
+            }
+        }
+        return targetLockPath.substring(0, targetLockPath.lastIndexOf(anno.keySeparator()));
+    }
 
-	@NoArgsConstructor
-	private static class LockInvocation {
+    private Map<String, Object> buildMethodParameterMap(String[] parameterNames, Object[] args) {
+        Map<String, Object> result = new HashMap<>(parameterNames.length);
+        int index = 0;
+        for (String parameterName : parameterNames) {
+            result.put(parameterName, args[index]);
+            index++;
+        }
+        return result;
+    }
 
-		@Getter
-		private String lockPathToUse;
-		@Getter
-		private long waitTime;
-		@Getter
-		private long leaseTime;
-		@Getter
-		private TimeUnit unit;
-		@Getter
-		private boolean isFair;
-		@Getter
-		private int order;
+    private boolean processLockChainAcquire(List<LockInvocation> lockInvocations) {
+        return lockInvocations.stream().allMatch(distributedLockInvocation ->
+                !distributedLockInvocation.isHeldByCurrentThread() && distributedLockInvocation.tryLock());
+    }
 
-		private RLock rLock;
+    private void processLockChainRelease(List<LockInvocation> lockInvocations) {
+        Collections.reverse(lockInvocations);
+        lockInvocations.forEach(LockInvocation::release);
+    }
 
-		public LockInvocation(String lockPathToUse, long waitTime, long leaseTime, TimeUnit unit,
-							  boolean isFair, int order, RedissonLockFactory lockFactory) {
-			this.lockPathToUse = lockPathToUse;
-			this.waitTime = waitTime;
-			this.leaseTime = leaseTime;
-			this.unit = unit;
-			this.isFair = isFair;
-			this.order = order;
-			this.rLock = lockFactory.createLockInstance(lockPathToUse, this.isFair);
-		}
+    private void registerRedissonLockAspect(Advice advice, DefaultListableBeanFactory beanFactory) {
+        AspectJExpressionPointcutAdvisor redissonLockAspect = new AspectJExpressionPointcutAdvisor();
+        redissonLockAspect.setLocation("$$redissonLockAspect##");
+        redissonLockAspect.setExpression("@annotation(org.throwable.redisson.annotation.RedissonDistributedLock) || @annotation(org.throwable.redisson.annotation.RedissonDistributedLocks)");
+        redissonLockAspect.setAdvice(advice);
+        redissonLockAspect.setBeanFactory(beanFactory);
+        beanFactory.registerSingleton("redissonLockAspect", redissonLockAspect);
+    }
 
-		public boolean tryLock() {
-			try {
-				return rLock.tryLock(waitTime, leaseTime, unit);
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-			return false;
-		}
+    @NoArgsConstructor
+    private static class LockInvocation {
 
-		public void release() {
-			try {
-				rLock.unlock();
-			} catch (IllegalMonitorStateException e) {
-				if (log.isWarnEnabled()) {
-					log.warn("Release lock error!", e);
-				}
-			}
-		}
+        @Getter
+        private String lockPathToUse;
+        @Getter
+        private long waitTime;
+        @Getter
+        private long leaseTime;
+        @Getter
+        private TimeUnit unit;
+        @Getter
+        private boolean isFair;
+        @Getter
+        private int order;
 
-		public boolean isLocked() {
-			return rLock.isLocked();
-		}
+        private RLock rLock;
 
-		public boolean isHeldByCurrentThread() {
-			return rLock.isHeldByCurrentThread();
-		}
+        public LockInvocation(String lockPathToUse, long waitTime, long leaseTime, TimeUnit unit,
+                              boolean isFair, int order, RedissonLockFactory lockFactory) {
+            this.lockPathToUse = lockPathToUse;
+            this.waitTime = waitTime;
+            this.leaseTime = leaseTime;
+            this.unit = unit;
+            this.isFair = isFair;
+            this.order = order;
+            this.rLock = lockFactory.createLockInstance(lockPathToUse, this.isFair);
+        }
 
-		public void forceUnlock() {
-			rLock.forceUnlock();
-		}
-	}
+        public boolean tryLock() {
+            try {
+                return rLock.tryLock(waitTime, leaseTime, unit);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return false;
+        }
+
+        public void release() {
+            try {
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                if (log.isWarnEnabled()) {
+                    log.warn("Release lock error!", e);
+                }
+            }
+        }
+
+        public boolean isLocked() {
+            return rLock.isLocked();
+        }
+
+        public boolean isHeldByCurrentThread() {
+            return rLock.isHeldByCurrentThread();
+        }
+
+        public void forceUnlock() {
+            rLock.forceUnlock();
+        }
+    }
 }
--- slime-distributed-lock-redisson/src/main/java/org/throwable/redisson/support/RedissonLockFactory.java
+++ slime-distributed-lock-redisson/src/main/java/org/throwable/redisson/support/RedissonLockFactory.java
@@ -6,12 +6,8 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-import org.throwable.redisson.configuration.RedissonLockProperties;
 import org.throwable.redisson.exception.RedissoninitializationException;
 
 import java.io.InputStream;
@@ -23,44 +19,41 @@ import java.io.InputStream;
  * @since 2017/6/25 2:18
  */
 @Slf4j
-@Component
-public class RedissonLockFactory implements InitializingBean, DisposableBean {
+public class RedissonLockFactory implements DisposableBean {
 
-	@Autowired
-	private RedissonLockProperties redissonLockProperties;
+    private String location;
+    private RedissonClient client;
 
-	private RedissonClient client;
+    public RedissonLockFactory(String location) {
+        this.location = location;
+        initRedissonClient();
+    }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		initRedissonClient();
-	}
+    @Override
+    public void destroy() throws Exception {
+        if (null != client && !client.isShutdown()) {
+            client.shutdown();
+        }
+    }
 
-	@Override
-	public void destroy() throws Exception {
-		if (null != client && !client.isShutdown()) {
-			client.shutdown();
-		}
-	}
+    private void initRedissonClient() {
+        String location = this.location;
+        Assert.hasText(location, "RedissonLockProperties field location must not be empty!");
+        try {
+            InputStream inputStream = new ClassPathResource(location).getInputStream();
+            Config config = Config.fromYAML(inputStream);
+            client = Redisson.create(config);
+        } catch (Exception e) {
+            log.error("Initialize redisson client failed!!!!", e);
+            throw new RedissoninitializationException(e);
+        }
+    }
 
-	private void initRedissonClient() throws Exception {
-		String location = redissonLockProperties.getLocation();
-		Assert.hasText(location, "RedissonLockProperties field location must not be empty!");
-		try {
-			InputStream inputStream = new ClassPathResource(location).getInputStream();
-			Config config = Config.fromYAML(inputStream);
-			client = Redisson.create(config);
-		} catch (Exception e) {
-			log.error("Initialize redisson client failed!!!!", e);
-			throw new RedissoninitializationException(e);
-		}
-	}
-
-	public RLock createLockInstance(String realLockPath, boolean isFair) {
-		if (isFair) {
-			return client.getFairLock(realLockPath);
-		} else {
-			return client.getLock(realLockPath);
-		}
-	}
+    public RLock createLockInstance(String realLockPath, boolean isFair) {
+        if (isFair) {
+            return client.getFairLock(realLockPath);
+        } else {
+            return client.getLock(realLockPath);
+        }
+    }
 }
